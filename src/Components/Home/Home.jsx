e8a6f196f0bd81ac607cdd6b8ec9f311e0e3ed2c
arminCicic <armincicic@gmail.com>
2022-05-28 15:47:53 +0200

    Updated UI

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppState } from "../../Context";
 import SingleProduct from "../SingleProduct/SingleProduct";
 import "./Home.css";
 import CoverText from "../CoverText/CoverText";
 import Footer from "../Footer/Footer";
+import NoResults from "../NoResults";
 
 const Home = () => {
   let { products, searchTerm } = AppState();
+  const { noResults, setNoResults } = useState(true);
 
   return (
     <>
       <CoverText />
+
       <div className="products-container">
         {products
           .filter((value) => {
@@ -26,6 +29,7 @@ const Home = () => {
             <SingleProduct prod={prod} key={prod.url} />
           ))}
       </div>
+
       <Footer />
     </>
   );
